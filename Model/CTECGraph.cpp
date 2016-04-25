2ebbdd8578cac2fe473ecbc0f1832f957effa24d
Sam Montoya <smon7958@740s-pl-118.local>
2016-04-25 12:08:47 -0600

    Fixed the class. Adding the graph header

--- Model/CTECGraph.cpp
+++ Model/CTECGraph.cpp
@@ -5,22 +5,6 @@
 //  Created by Montoya, Samuel on 4/25/16.
 //  Copyright © 2016 CTEC. All rights reserved.
 //
-namespace CTECData {
-    template <class Type>
-    class CTECGraph
-    {
-    private:
-        static const int MAXIMUM = 20;
-        bool adjacencyMatrix [MAXIMUM] [MAXIMUM];
-        Type labels [MAXIMUM];
-        int manyVertices;
-        
-    public:
-        CTECGraph();
-        ~CTECGraph();
-        
-        void addVertex(const Type& value);
-        
-    };
-}
+
 #include "CTECGraph.hpp"
+}
